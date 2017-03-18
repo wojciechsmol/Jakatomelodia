7f96010947d4dea1ff522f30b3278497cdf49056
wojciechsmol <wojciechsmolcontact@gmail.com>
2017-03-18 17:17:10 +0100

    Initial commit

---
+++ app/src/main/java/pl/wojciech/smol/jakatomelodia/MainActivity.java
@@ -0,0 +1,14 @@
+package pl.wojciech.smol.jakatomelodia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
